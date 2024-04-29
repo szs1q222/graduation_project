bbe7a5ba9ee97e6a53ad94b2bdacb68a4acbdc41
szs1q222 <1640643989@qq.com>
2024-04-29 15:22:23 +0800

    refactor: 函数化分割，合并数据集操作

--- graduation_project_program/dataset/merge_dataset.py
+++ graduation_project_program/dataset/merge_dataset.py
@@ -1,20 +1,33 @@
 import os
 import shutil
 
-# 训练集切分比例
-train_rate = 0.9
-# 原始训练集集地址
-train_address = "./train"
-# 目标测试集地址
-test_address = "./test"
 
-# 测试集地址中的所有文件合并到训练集中
-for i in range(int(7680 * train_rate) + 1, 7680 + 1):
-    shutil.move(f"{test_address}/img/{i}.jpg", f"{train_address}/img/{i}.jpg")
-    shutil.move(f"{test_address}/label/{i}.txt", f"{train_address}/label/{i}.txt")
-for i in range(int(5100 * train_rate) + 1, 5100 + 1):
-    shutil.move(f"{test_address}/img/red_{i}.jpg", f"{train_address}/img/red_{i}.jpg")
-    shutil.move(f"{test_address}/label/red_{i}.txt", f"{train_address}/label/red_{i}.txt")
+def merge_dataset(train_rate: float, train_address: str, test_address: str):
+    """
+    合并训练集和测试集
+    :param train_rate: 训练集切分比例
+    :param train_address: 训练集地址
+    :param test_address: 测试集地址
+    :return:
+    """
+    # 测试集地址中的所有文件合并到训练集中
+    for i in range(int(7680 * train_rate) + 1, 7680 + 1):
+        shutil.move(f"{test_address}/img/{i}.jpg", f"{train_address}/img/{i}.jpg")
+        shutil.move(f"{test_address}/label/{i}.txt", f"{train_address}/label/{i}.txt")
+    for i in range(int(5100 * train_rate) + 1, 5100 + 1):
+        shutil.move(f"{test_address}/img/red_{i}.jpg", f"{train_address}/img/red_{i}.jpg")
+        shutil.move(f"{test_address}/label/red_{i}.txt", f"{train_address}/label/red_{i}.txt")
 
-# 打印完成信息
-print("File transfer completed.")
+
+if __name__ == '__main__':
+    # 训练集切分比例
+    train_rate = 0.9
+    # 原始训练集集地址
+    train_address = "./train"
+    # 目标测试集地址
+    test_address = "./test"
+
+    merge_dataset(train_rate, train_address, test_address)
+
+    # 打印完成信息
+    print("File transfer completed.")
--- graduation_project_program/dataset/segmentation_dataset.py
+++ graduation_project_program/dataset/segmentation_dataset.py
@@ -1,28 +1,41 @@
 import os
 import shutil
 
-# 训练集切分比例
-train_rate = 0.9
-# 原始训练集集地址
-train_address = "./train"
-# 目标测试集地址
-test_address = "./test"
 
-# 确保测试集地址存在
-if not os.path.exists(test_address):
-    os.makedirs(test_address)
-if not os.path.exists(f"{test_address}/img"):
-    os.makedirs(f"{test_address}/img")
-if not os.path.exists(f"{test_address}/label"):
-    os.makedirs(f"{test_address}/label")
+def segmentation_dataset(train_rate: float, train_address: str, test_address: str):
+    """
+    分割原始训练集
+    :param train_rate: 训练集切分比例
+    :param train_address: 训练集地址
+    :param test_address: 测试集地址
+    :return:
+    """
+    # 确保测试集地址存在
+    if not os.path.exists(test_address):
+        os.makedirs(test_address)
+    if not os.path.exists(f"{test_address}/img"):
+        os.makedirs(f"{test_address}/img")
+    if not os.path.exists(f"{test_address}/label"):
+        os.makedirs(f"{test_address}/label")
 
-# 获取原始训练集地址中的所有文件(7680张,5100张)
-for i in range(int(7680 * train_rate) + 1, 7680 + 1):
-    shutil.move(f"{train_address}/img/{i}.jpg", f"{test_address}/img/{i}.jpg")
-    shutil.move(f"{train_address}/label/{i}.txt", f"{test_address}/label/{i}.txt")
-for i in range(int(5100 * train_rate) + 1, 5100 + 1):
-    shutil.move(f"{train_address}/img/red_{i}.jpg", f"{test_address}/img/red_{i}.jpg")
-    shutil.move(f"{train_address}/label/red_{i}.txt", f"{test_address}/label/red_{i}.txt")
+    # 获取原始训练集地址中的所有文件(7680张,5100张)
+    for i in range(int(7680 * train_rate) + 1, 7680 + 1):
+        shutil.move(f"{train_address}/img/{i}.jpg", f"{test_address}/img/{i}.jpg")
+        shutil.move(f"{train_address}/label/{i}.txt", f"{test_address}/label/{i}.txt")
+    for i in range(int(5100 * train_rate) + 1, 5100 + 1):
+        shutil.move(f"{train_address}/img/red_{i}.jpg", f"{test_address}/img/red_{i}.jpg")
+        shutil.move(f"{train_address}/label/red_{i}.txt", f"{test_address}/label/red_{i}.txt")
 
-# 打印完成信息
-print("File transfer completed.")
+
+if __name__ == '__main__':
+    # 训练集切分比例
+    train_rate = 0.9
+    # 原始训练集集地址
+    train_address = "./train"
+    # 目标测试集地址
+    test_address = "./test"
+
+    segmentation_dataset(train_rate=train_rate, train_address=train_address, test_address=test_address)
+
+    # 打印完成信息
+    print("File transfer completed.")
